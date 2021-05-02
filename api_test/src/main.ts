cbcb0842b8342c29b9119ef53300a97a2aa64660
Daniel Rodriguez <drg809@gmail.com>
2021-05-02 23:37:13 +0200

    dependencia para comprimir las llamadas y carga en main.ts para ver si se usa o no

--- api_test/src/main.ts
+++ api_test/src/main.ts
@@ -1,11 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn'],
   });
+
+  app.use(compression());
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
